1dccea88273fd7f6ac037fa1aa5fdd69e469a102
Matthew Phillips <phillips.matthew829@gmail.com>
2022-02-02 14:10:15 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,12 +6,12 @@ function Movies(){
     <h1>Movies Page</h1>
     {movies.map((movie) => (
       <div>
-        <h3>{movie.title}</h3>
-        <h3>{movie.time}</h3>
+        <h3>Movie Title: <i>{movie.title}</i></h3>
+        <h3>Time: {movie.time} minutes</h3>
         <ul> Genres:
-          {movies.map((genres) => (
+          {movie.genres.map((genre) => (
             <li key={movie.genre}>
-              {movies.genres}
+              {genre}
             </li>
           ))}
         </ul>
